edfc40b8f9f5e941ee952c7af3cc469634fdff6b
Arniyu <arnih14@ru.is>
2015-05-07 10:47:26 +0000

    Breytti textann i LoginPartial

---
+++ Hello_World.cpp/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "Hello world!" << endl;
+    cout << "Please Enter ur Name: " << endl;
+    cin >> name;
+
+    cout << "Ur Name is: "<< name << endl;
+    cout << "Good-Bye" << endl;
+    cout << " :P :P :P " << endl;
+
+
+    return 0;
+}
